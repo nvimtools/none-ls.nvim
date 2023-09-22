2782d44f6cb243c60d397a21a9902bc328eafb94
Zephyr Lykos <git@mochaa.ws>
2023-09-22 13:01:14 +0800

    cspell: port from davidmh/cspell.nvim
    Co-authored-by: Ettienne Pitts <Sloff@users.noreply.github.com>
    Co-authored-by: Fritiof Rusck <fritiof@rusck.se>
    Co-authored-by: Johann Weging <1328109+yoo@users.noreply.github.com>
    Co-authored-by: Jose Alvarez <j.alvarez11@icloud.com>
    Co-authored-by: Juan Barajas <jbperches@gmail.com>
    Co-authored-by: Mariano Zunino <marianoz@posteo.net>
    Co-authored-by: Tobias Schmitz <tobiasschmitz2001@gmail.com>
    Co-authored-by: mtoohey31 <mtoohey31@users.noreply.github.com>
    Co-authored-by: David Mejorado <david.mejorado@gmail.com>
    Co-authored-by: Minwei Shen <shenminwei@bytedance.com>

--- lua/null-ls/builtins/diagnostics/cspell.lua
+++ lua/null-ls/builtins/diagnostics/cspell.lua
@@ -1,5 +1,6 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
+local helpers = require("null-ls.helpers.cspell")
 
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 
@@ -21,37 +22,57 @@ local custom_user_data = {
     end,
 }
 
+local needs_warning = true
+
 return h.make_builtin({
     name = "cspell",
     meta = {
         url = "https://github.com/streetsidesoftware/cspell",
-        description = [[cspell is a spell checker for code.
-
-**This source is not actively developed in this repository.**
-
-An up-to-date version exists as a companion plugin in [cspell.nvim](https://github.com/davidmh/cspell.nvim)
-]],
+        description = "cspell is a spell checker for code.",
     },
     method = DIAGNOSTICS,
     filetypes = {},
     generator_opts = {
         command = "cspell",
+        ---@param params GeneratorParams
         args = function(params)
+            params.cwd = params.cwd or vim.loop.cwd()
+
             local cspell_args = {
                 "lint",
                 "--language-id",
                 params.ft,
-                "stdin",
+                "stdin://" .. params.bufname,
             }
 
-            -- only enable suggestions when using the code actions built-in, since they slow down the command
-            local should_add_suggestions = not vim.tbl_isempty(require("null-ls").get_source({
+            local config_path = helpers.get_config_path(params)
+            if config_path then
+                cspell_args = vim.list_extend({ "-c", config_path }, cspell_args)
+            end
+
+            local code_action_source = require("null-ls.sources").get({
                 name = "cspell",
                 method = methods.internal.CODE_ACTION,
-            }))
+            })[1]
 
-            if should_add_suggestions then
+            if code_action_source ~= nil then
+                -- only enable suggestions when using the code actions built-in, since they slow down the command
                 cspell_args = vim.list_extend({ "--show-suggestions" }, cspell_args)
+
+                local code_action_config = code_action_source.config or {}
+                local diagnostics_config = params and params:get_config() or {}
+
+                if helpers.matching_configs(code_action_config, diagnostics_config) then
+                    -- warm up the config cache so we have the config ready by the time we call the code action
+                    helpers.async_get_config_info(params)
+                elseif needs_warning then
+                    needs_warning = false
+                    vim.notify(
+                        "You should use the same config for both sources",
+                        vim.log.levels.WARN,
+                        { title = "cspell.nvim" }
+                    )
+                end
             end
 
             return cspell_args
