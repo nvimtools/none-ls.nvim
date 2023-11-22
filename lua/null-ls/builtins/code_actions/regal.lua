7293b3f0df87816e29857524678242255cee961e
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2023-11-22 19:28:54 +0200

    fix(regal): code action to ignore violations via config file (#28)
    Signed-off-by: Ievgenii Shepeliuk <eshepelyuk@gmail.com>

--- lua/null-ls/builtins/code_actions/regal.lua
+++ lua/null-ls/builtins/code_actions/regal.lua
@@ -1,7 +1,10 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
+local logger = require("null-ls.logger")
+local Job = require("plenary.job")
 
 local CODE_ACTION = methods.internal.CODE_ACTION
+local REGAL_CFG = ".regal/config.yaml"
 
 local line_diagnostics = function(params)
     local col = params.col
@@ -13,6 +16,46 @@ local line_diagnostics = function(params)
         end
     end
     return diagnostics
+end
+
+local ignore_inline_action = function(params, d, prev_ln, prev_ign, indent)
+    return {
+        title = "Ignore Regal rule '" .. d.code .. "' for this line",
+        action = function()
+            local ign = prev_ign == nil and indent .. "# regal ignore:" or prev_ln .. ","
+            local lnum = prev_ign == nil and d.lnum or d.lnum - 1
+
+            vim.api.nvim_buf_set_lines(params.bufnr, lnum, d.lnum, false, { ign .. d.code })
+            vim.api.nvim_command("write")
+        end,
+    }
+end
+
+local ignore_config_action = function(d)
+    return {
+        title = "Ignore Regal rule '" .. d.code .. "' via " .. REGAL_CFG,
+        action = function()
+            Job:new({
+                command = "yq",
+                args = {
+                    "-i",
+                    ".rules." .. d.user_data.category .. "." .. d.code .. '.level|="ignore"',
+                    REGAL_CFG,
+                },
+                on_exit = function(j, return_val)
+                    if return_val ~= 0 then
+                        logger:error(j:stderr_result()[1])
+                    else
+                        vim.notify(
+                            "'" .. d.code .. "' ignored in " .. REGAL_CFG,
+                            vim.log.levels.INFO,
+                            { title = "none-ls" }
+                        )
+                    end
+                end,
+            }):start()
+        end,
+    }
 end
 
 local code_actions_handler = function(params)
@@ -28,16 +71,8 @@ local code_actions_handler = function(params)
     local _, _, prev_ign = prev_ln:find("#%s+regal%s+ignore:([%a%-,]+)")
 
     for _, d in ipairs(diagnostics) do
-        table.insert(actions, {
-            title = "Ignore Regal rule " .. d.code .. " for this line",
-            action = function()
-                local ign = prev_ign == nil and indent .. "# regal ignore:" or prev_ln .. ","
-                local lnum = prev_ign == nil and d.lnum or d.lnum - 1
-
-                vim.api.nvim_buf_set_lines(params.bufnr, lnum, d.lnum, false, { ign .. d.code })
-                vim.api.nvim_command("write")
-            end,
-        })
+        table.insert(actions, ignore_inline_action(params, d, prev_ln, prev_ign, indent))
+        table.insert(actions, ignore_config_action(d))
     end
 
     return actions
