023ccc3a85b1ed0bb0cf4b5f744092d8bdaaa388
raBeta <raul.bethencourt.pro@gmail.com>
2024-01-02 13:15:44 +0100

    formatting: add pretty-php (#54)
    https://github.com/lkrms/pretty-php

---
+++ lua/null-ls/builtins/formatting/pretty_php.lua
@@ -0,0 +1,20 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "pretty_php",
+    meta = {
+        url = "https://github.com/lkrms/pretty-php",
+        description = "The opinionated PHP code formatter.",
+    },
+    method = FORMATTING,
+    filetypes = { "php" },
+    generator_opts = {
+        command = "pretty-php",
+        args = { "$FILENAME" },
+        stdin = false,
+    },
+    factory = h.formatter_factory,
+})
