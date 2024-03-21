63ae101b8f82a9b0b942587e04fedd68553a4d50
Maciej Litwiniuk <maciej@litwiniuk.net>
2024-03-21 12:38:43 +0100

    Adds htmlbeautifier formatter - for eruby files (#101)
    * Adds htmlbeautifier formatter - for eruby files
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ lua/null-ls/builtins/formatting/htmlbeautifier.lua
@@ -0,0 +1,23 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "htmlbeautifier",
+    meta = {
+        url = "https://github.com/threedaymonk/htmlbeautifier",
+        description = "A normaliser/beautifier for HTML that also understands embedded Ruby. Ideal for tidying up Rails templates.",
+    },
+    method = FORMATTING,
+    filetypes = { "eruby" },
+    generator_opts = {
+        command = "htmlbeautifier",
+        args = {
+            "$FILENAME",
+        },
+        to_stdin = false,
+        to_temp_file = true,
+    },
+    factory = h.formatter_factory,
+})
