309a1f651ec3e9ba0e3e72c4ed4ae7ec215e34ce
Gaurang Shah <gaurangnshah@gmail.com>
2024-07-24 19:43:11 -0400

    fix: terragrunt_fmt formatter for formatting hcl files (#167)

--- lua/null-ls/builtins/formatting/terragrunt_fmt.lua
+++ lua/null-ls/builtins/formatting/terragrunt_fmt.lua
@@ -10,14 +10,15 @@ return h.make_builtin({
         description = "The terragrunt hclfmt command rewrites `terragrunt` configuration files to a canonical format and style.",
     },
     method = FORMATTING,
+    to_temp_file = true,
+    from_temp_file = true,
     filetypes = { "hcl" },
     generator_opts = {
         command = "terragrunt",
         args = {
             "hclfmt",
-            "-",
+            "$FILENAME",
         },
-        to_stdin = true,
     },
     factory = h.formatter_factory,
 })
