bd1fdbb2d0a028c7edc4b41ddef4415ac43bd4de
Zephyr Lykos <git@mochaa.ws>
2025-01-06 22:32:36 +0800

    helpers/check_exit_code: add option to check silently

--- lua/null-ls/helpers/init.lua
+++ lua/null-ls/helpers/init.lua
@@ -1,5 +1,6 @@
 return {
     cache = require("null-ls.helpers.cache"),
+    check_exit_code = require("null-ls.helpers.check_exit_code"),
     diagnostics = require("null-ls.helpers.diagnostics"),
     formatter_factory = require("null-ls.helpers.formatter_factory"),
     generator_factory = require("null-ls.helpers.generator_factory"),
