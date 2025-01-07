cc810f70852bd37334fad658372a8cfedd1da4e8
Barrett Ruth <62671086+barrett-ruth@users.noreply.github.com>
2025-01-07 04:45:34 -0500

    helpers/factory: default formatter valid exit code to 0 (#221)
    Co-authored-by: Zephyr Lykos <git@mochaa.ws>

--- lua/null-ls/helpers/formatter_factory.lua
+++ lua/null-ls/helpers/formatter_factory.lua
@@ -1,9 +1,10 @@
 return function(opts)
+    local h = require("null-ls.helpers")
+
     -- ignore errors unless otherwise specified
     if opts.ignore_stderr == nil then
         opts.ignore_stderr = true
     end
-
     -- for formatters, to_temp_file only works if from_temp_file is also set
     if opts.to_temp_file then
         opts.from_temp_file = true
@@ -18,5 +19,10 @@ return function(opts)
         return done({ { text = output } })
     end
 
-    return require("null-ls.helpers").generator_factory(opts)
+    if opts.check_exit_code == nil then
+        opts.check_exit_code = 0
+    end
+    opts.check_exit_code = h.check_exit_code(opts.check_exit_code, false, opts.command)
+
+    return h.generator_factory(opts)
 end
