1b753fc8f2a9b904aae43901897a551584d32711
Zephyr Lykos <git@mochaa.ws>
2025-01-08 17:16:36 +0800

    fixup! helpers/factory: default formatter valid exit code to 0 (#221)

--- lua/null-ls/helpers/generator_factory.lua
+++ lua/null-ls/helpers/generator_factory.lua
@@ -132,10 +132,14 @@ return function(opts)
         opts.temp_dir,
         opts.prepend_extra_args
 
-    if type(check_exit_code) == "table" then
-        local codes = check_exit_code
+    local codes = check_exit_code
+    if type(codes) == "table" then
         check_exit_code = function(code)
             return vim.tbl_contains(codes, code)
+        end
+    elseif type(codes) == "number" then
+        check_exit_code = function(code)
+            return code <= codes
         end
     end
 
