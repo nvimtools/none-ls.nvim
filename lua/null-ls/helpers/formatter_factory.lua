df3489df517c0c2069c45b23c6cc1cfe579f2f53
Barrett Ruth <br.barrettruth@gmail.com>
2025-04-09 16:51:52 -0400

    fix(#257): remove lass vim.validate times

--- lua/null-ls/helpers/formatter_factory.lua
+++ lua/null-ls/helpers/formatter_factory.lua
@@ -1,6 +1,4 @@
 return function(opts)
-    local h = require("null-ls.helpers")
-
     -- ignore errors unless otherwise specified
     if opts.ignore_stderr == nil then
         opts.ignore_stderr = true
@@ -18,11 +16,4 @@ return function(opts)
 
         return done({ { text = output } })
     end
-
-    if opts.check_exit_code == nil then
-        opts.check_exit_code = 0
-    end
-    opts.check_exit_code = h.check_exit_code(opts.check_exit_code, false, opts.command)
-
-    return h.generator_factory(opts)
 end
