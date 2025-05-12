0dd178621dd1059b6d90f59022df5601c7912695
Zephyr Lykos <git@mochaa.ws>
2025-05-12 11:37:50 +0800

    helpers/cache: always return a new copy
    closes #285

--- lua/null-ls/helpers/cache.lua
+++ lua/null-ls/helpers/cache.lua
@@ -33,13 +33,13 @@ M.by_bufnr = function(cb)
             M.cache[key][bufnr] = cb(params) or false
         end
 
-        return M.cache[key][bufnr]
+        return vim.deepcopy(M.cache[key][bufnr])
     end
 end
 
 --- creates a function that caches the output of an async callback, indexed by bufnr
 ---@param cb function
----@return fun(params: NullLsCacheParams): any
+---@return fun(params: NullLsCacheParams, done: function): any
 M.by_bufnr_async = function(cb)
     -- assign next available key, since we just want to avoid collisions
     local key = next_key
@@ -53,17 +53,17 @@ M.by_bufnr_async = function(cb)
             -- make sure we always store a value so we know we've already called cb
             cb(params, function(result)
                 M.cache[key][bufnr] = result or false
-                done(M.cache[key][bufnr])
+                done(vim.deepcopy(M.cache[key][bufnr]))
             end)
         else
-            done(M.cache[key][bufnr])
+            done(vim.deepcopy(M.cache[key][bufnr]))
         end
     end
 end
 
 --- creates a function that caches the output of an async callback, indexed by project root
 ---@param cb function
----@return fun(params: NullLsParams): any
+---@return fun(params: NullLsParams, done: function): any
 M.by_bufroot_async = function(cb)
     -- assign next available key, since we just want to avoid collisions
     local key = next_key
@@ -72,15 +72,16 @@ M.by_bufroot_async = function(cb)
 
     return function(params, done)
         local root = params.root
+        assert(root, "root should not be empty when caching by bufroot")
         -- if we haven't cached a value yet, get it from cb
         if M.cache[key][root] == nil then
             -- make sure we always store a value so we know we've already called cb
             cb(params, function(result)
                 M.cache[key][root] = result or false
-                done(M.cache[key][root])
+                done(vim.deepcopy(M.cache[key][root]))
             end)
         else
-            done(M.cache[key][root])
+            done(vim.deepcopy(M.cache[key][root]))
         end
     end
 end
