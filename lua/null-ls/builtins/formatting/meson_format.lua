071da2e04aeb452dae22a4930cbff1f6d90f1edf
Zephyr Lykos <git@mochaa.ws>
2025-08-06 04:21:38 +0800

    builtins/meson_format: initialize support for native meson format

---
+++ lua/null-ls/builtins/formatting/meson_format.lua
@@ -0,0 +1,20 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "meson_format",
+    meta = {
+        url = "https://mesonbuild.com/Commands.html#format",
+        description = "Meson's builtin formatter",
+    },
+    method = FORMATTING,
+    filetypes = { "meson" },
+    generator_opts = {
+        command = "meson",
+        args = { "format", "--", "$FILENAME" },
+        to_stdin = true,
+    },
+    factory = h.formatter_factory,
+})
